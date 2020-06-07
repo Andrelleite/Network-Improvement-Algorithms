fd9a8ea61c07910dd3347e160517763604c7b5fb
André Leite <leiteandre4@gmail.com>
2020-06-07 03:40:37 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define MAX 1024
+#define MAX 128
+#define TOP 20
 #define INF 999999
 
 int V = 0;
@@ -11,9 +12,12 @@ int time = 0;
 int NServers = 0;
 int Cable = 0;
 int Topology = 0;
+int Networks = 0;
+int *card;
 
 struct node{
 
+    int serv;
     int src;
     int val;
     int cost;
@@ -28,32 +32,50 @@ struct graph{
 
 };
 
+struct subset{
+    int parent;
+    int rank;
+};
+
+
+/** ------------------------------ MISC (PRINTS AND UTIL) ---------------------------- **/
+
+void printSolution(int **dist,struct graph *g){
+    int i, j;
+    printf ("The following matrix shows the shortest distances"
+            " between every pair of vertices \n");
+    for ( i = 1; i < g->v; i++){
+        for ( j = 1; j < g->v; j++){
+            if (dist[i][j] == INF)
+                printf("%5s", "INF");
+            else
+                printf ("%5d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int min(int a,int b){
     return(a<b?a:b);
 }
 
 void printServers(struct graph *s){
 
-    int i = 0;
-    struct node *temp;
+    int i = 1;
     for(;i<s->v;i++){
-        printf("___SERVER %d: ",s->arr[i]->src);
-        temp = s->arr[i];
-        while(temp!=NULL){
-            printf("%d(%d) ",temp->val,temp->cost);
-            temp = temp->next;
+        if(s->arr[i]->serv){
+            printf("___SERVER %d: ",s->arr[i]->src);
+        }else{
+            printf("___EQUIPMENT %d: ",s->arr[i]->src);
         }
         printf("\n");
     }
 
 }
 
-void printArr(int dist[], int n){
-    int i;
-    printf("Vertex   Distance from Source\n");
-    for ( i = 0; i < n; ++i)
-        printf("%d \t\t %d\n", i, dist[i]);
-}
+
+/** ------------------------------ FASE 0 (GRAPH UTIL) ---------------------------- **/
+
 
 struct graph* createGraph(int v){
 
@@ -74,6 +96,7 @@ void addEdge(struct graph* g,int u,int v,int c){
 
     struct node* temp =(struct node*)malloc(sizeof(struct node));
     struct node* temp2 =(struct node*)malloc(sizeof(struct node));
+    temp->serv = temp2->serv = 0;
     temp->src = u;
     temp2->src = v;
     temp->val = v;
@@ -85,6 +108,9 @@ void addEdge(struct graph* g,int u,int v,int c){
     g->arr[v] = temp2;
 
 }
+
+/** -------------------------------FASE 1 (SERVERS) ---------------------------- **/
+
 
 void ArPointsRec(struct graph * g,int node,int* isVisited,int* des,int* parent,int* low,int* ap){
 
@@ -138,10 +164,10 @@ struct graph *ArPoints(struct graph* g, int *n){
 
     for(i=0;i<g->v;i++){
         if(ap[i]==1){
+            g->arr[i]->serv = 1;
             NServers++;
         }
     }
-
     if(NServers > 1){
         j = 0;
         servers =(struct graph*)malloc(sizeof(struct graph));
@@ -161,12 +187,48 @@ struct graph *ArPoints(struct graph* g, int *n){
 
 }
 
-void floydWarshall (struct graph* g, struct graph* sr) {
+int **countNetworks(int **dist, struct graph *g){
 
+    int i,j;
+    int c = 1;
+    int **net = (int **)malloc(sizeof(int *)*g->v);
+    card = (int *)calloc(g->v,sizeof(int));
+
+    for(i=0;i<g->v;i++){
+        net[i] = (int *)calloc(g->v,sizeof(int));
+    }
+    net[0][0] = g->arr[1]->src;
+    card[0]++;
+
+    for(i = 2; i < g->v; i++){
+        j = 0;
+        while(j <= c){
+            if(dist[net[j][0]][i]!=INF && net[j][0]!=0){
+                net[j][card[j]] = i;
+                card[j]++;
+                j = c+1;
+            }else if(net[j][0]==0){
+                net[j][0] = i;
+                card[j]++;
+                j = c+1;
+                c++;
+            }
+            j++;
+        }
+    }
+    Networks = c;
+    return net;
+
+}
+
+/** ------------------------------ FASE 2 (FULLY CONNECTED) ---------------------------- **/
+
+int  **floydWarshall(struct graph* g, struct graph* sr) {
+
+    int i, j, k;
     int **dist = (int **)malloc(sizeof(int *)*g->v);
     int **graph = (int **)malloc(sizeof(int *)*g->v);
     struct node *temp;
-    int i, j, k;
 
     for(i = 0; i < g->v; i++){
         dist[i] = (int *)malloc(sizeof(int)*g->v);
@@ -194,6 +256,9 @@ void floydWarshall (struct graph* g, struct graph* sr) {
     for (i = 1; i < g->v; i++){
         for (j = 1; j < g->v; j++){
             dist[i][j] = graph[i][j];
+            if(graph[i][j]==INF){
+                graph[i][j]=0;
+            }
         }
     }
 
@@ -207,91 +272,170 @@ void floydWarshall (struct graph* g, struct graph* sr) {
         }
     }
 
-    for(i = 0; i < (sr->v) && Cable != -INF; i++){
-        if(i!=sr->v-1){
-            if(dist[sr->arr[i]->src][sr->arr[i+1]->src] != INF){
-                Cable += dist[sr->arr[i]->src][sr->arr[i+1]->src];
-            }else{
-                Cable = -INF;
-            }
+
+    /*printSolution(dist,g);*/
+    for(i = 0; i < (sr->v)-1 && Cable != -INF; i++){
+        for(j = i+1; j < (sr->v) && Cable != -INF; j++){
+                if(dist[sr->arr[i]->src][sr->arr[j]->src] != INF){
+                    Cable += dist[sr->arr[i]->src][sr->arr[j]->src];
+                }
         }
 
     }
     if(Cable == -INF){
         Cable = 0;
-    }else{
-        if(sr->v > 2){
-            Cable += dist[sr->arr[0]->src][sr->arr[(sr->v)-1]->src];
-        }
     }
+    return dist;
+}
 
+/** ------------------------------ FASE 3 (MINIMUM SPANNING TREE IN NETWORK) ---------------------------- **/
+
+void swap(struct node* a, struct node* b){
+    struct node t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int subset(struct node **arr, int low, int high){
+
+    int j;
+    struct node *pivot = arr[high];
+    int i = (low - 1);
+
+    for (j = low; j <= high- 1; j++){
+
+        if (arr[j]->cost < pivot->cost){
+            i++;
+            swap(arr[i], arr[j]);
+        }
+
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+void quickSort(struct node **arr, int low, int high){
+
+    if (low < high){
+        int pi = subset(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int find(struct subset subsets[], int i){
+
+    if (subsets[i].parent != i)
+        subsets[i].parent = find(subsets, subsets[i].parent);
+
+    return subsets[i].parent;
 
 }
 
-void bellmanFord(struct graph* g, int src, struct graph* sr, int *ses){
+void Union(struct subset subsets[], int x, int y){
 
-    int i, j, z, u ,v, weight;
-    int max = INF;
-    struct node *temp;
-    int *shortest = (int *)malloc(sizeof(int)*g->v);
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
 
-    for(z = 0; z < sr->v; z++){
+    if (subsets[xroot].rank < subsets[yroot].rank)
+        subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank)
+        subsets[yroot].parent = xroot;
 
-        src = sr->arr[z]->src;
-        for (i = 0; i < g->v; i++){
-            shortest[i] = INF;
-        }
-        shortest[src] = 0;
-
-
-        for (i = 2; i <= g->v-1; i++) {
-            for ( j = 1; j < g->v ; j++) {
-                if(g->arr[j] != NULL){
-                    temp = g->arr[j];
-                    u = g->arr[j]->src;
-                    while(temp != NULL){
-                        v = temp->val;
-                        weight = temp->cost;
-                        if (shortest[u] != INF && shortest[u] + weight < shortest[v]){
-                            shortest[v] = shortest[u] + weight;
-                        }
-                        temp = temp->next;
-                    }
-                    if(i == g->v-1){
-                        if(ses[u]!=0){
-                            if(shortest[u] < INF){
-                                Topology += shortest[u];
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        if(max > Topology && Topology != 0){
-            max = Topology;
-        }
-        Topology = 0;
+    else
+    {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
     }
-    if(max==INF){
-        max = 0;
-    }
-    Topology = max;
-    //printArr(dist, g->v);
+}
 
+int myComp(const void* a, const void* b){
+
+    struct node* a1 = (struct node*)a;
+    struct node* b1 = (struct node*)b;
+    return a1->cost > b1->cost;
+}
+
+void primMST(struct graph* graph, int V){
+
+    int v,x,y;
+    int v_max = V;
+    int i = 0;
+    int j = 0;
+    struct node *next_edge;
+    struct subset *subsets;
+    struct node **result;
+
+    if(V < graph->v){
+        v_max = graph->v;
+    }
+
+    subsets =(struct subset*) malloc( v_max * sizeof(struct subset) );
+    result = (struct node **)malloc( v_max * sizeof(struct node *));
+
+    for (v = 0; v < v_max; v++){
+        subsets[v].parent = v;
+        subsets[v].rank = 0;
+    }
+
+    while (j < graph->v - 1 && i < graph->v){
+        next_edge = graph->arr[i++];
+        x = find(subsets, next_edge->src);
+        y = find(subsets, next_edge->val);
+        if (x != y){
+            result[j] = (struct node*) malloc(sizeof(struct node));
+            result[j++] = next_edge;
+            Union(subsets, x, y);
+        }
+
+    }
+
+    for (i = 0; i < j; ++i){
+        if(result[i]->cost!=INF){
+            Topology+=result[i]->cost;
+        }
+    }
     return;
 }
+
+struct graph *transform(struct graph *sr, int v, int **dist){
+
+    int i,k,j;
+    struct node* novo;
+    struct graph *prims = createGraph(256);
+    j = 0;
+    for(i = 0; i < sr->v - 1; i++){
+        for(k = i+1 ;k < sr->v; k++){
+            novo = (struct node*)malloc(sizeof(struct node));
+            novo->src = sr->arr[i]->src;
+            novo->val = sr->arr[k]->src;
+            novo->cost = dist[sr->arr[i]->src][sr->arr[k]->src];
+            prims->arr[j] = novo;
+            j++;
+        }
+    }
+    prims->v = j;
+    quickSort(prims->arr,0,prims->v-1);
+    return prims;
+}
+
+/** --------------------------- [INITIALIZER] ----------------------------- **/
 
 void init(){
 
     int size;
     int links;
     int u, v, c;
-    char *token;
-    int *server_ez;
-    char input[MAX];
-    struct graph* adj;
 
+    char *token;
+    char input[MAX];
+
+    int *server_ez;
+    int **dist;
+
+    struct graph* adj;
     struct graph* sr;
+
     scanf("%[^\n]s", input);
 
     while (strcmp("0", input) != 0) {
@@ -320,23 +464,33 @@ void init(){
         server_ez = (int *)calloc(adj->v,sizeof(int));
         sr = ArPoints(adj,server_ez);
 
-        if(sr!=NULL);
-
         if(NServers){
             printf("%d ",NServers);
+
             if(NServers > 1){
 
+
                 /**It only is considered for an all-connected network**/
-                floydWarshall(adj,sr);
+                dist = floydWarshall(adj,sr);
+
                 if(Cable!=-INF){
                     printf("%d ",Cable);
                 }else{
                     printf("0 ");
                 }
                 Cable = 0;
+                /** End of FW **/
 
-                /**Bellman-Ford**/
-                bellmanFord(adj,sr->arr[0]->src,sr,server_ez);
+                /**Prim**/
+                if(sr->v > 2){
+                    sr = transform(sr,adj->v,dist);
+                    primMST(sr,adj->v);
+                }else{
+                    if(dist[sr->arr[0]->src][sr->arr[1]->src]!=INF){
+                        Topology = dist[sr->arr[0]->src][sr->arr[1]->src];
+                    }
+                }
+
                 if(Topology!=-INF){
                     printf("%d\n",Topology);
                 }else{
@@ -347,14 +501,14 @@ void init(){
             }else{
                 printf("0 0\n");
             }
+
         }else{
             printf("no server\n");
         }
-
         scanf(" %[^\n]s", input);
 
-        free(server_ez);
         free(sr);
+        free(server_ez);
         free(adj);
     }
 
